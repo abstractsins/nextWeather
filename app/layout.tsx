034c5d4858c68f3f09b7bb61842fbbc2d17ac83f
Daniel Berlin <daniel.berlin@comcast.net>
2025-08-26 23:37:36 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+
+        <link rel="manifest" href="/site.webmanifest" />
+
+        <meta name="theme-color" content="#ffffff" />
+
+      </head>
       <body className={`${monst.variable} ${zain.variable} ${geistSans.variable} ${geistMono.variable} antialiased`} >
         <WeatherProvider>
           {/* {process.env.NEXT_PUBLIC_ENV !== 'production' && } */}
